b4a9c236a1cb026997a4795e1b312fafc09244ac
stanhelsloot <43984796+stanhelsloot@users.noreply.github.com>
2018-11-12 16:12:07 +0100

    improved visualization

--- homework/week_2/eda.py
+++ homework/week_2/eda.py
@@ -2,6 +2,7 @@
 # 10762388
 import csv
 import pandas as pd
+import matplotlib.pyplot as plt
 
 RAW_INPUT = "input.csv"
 INPUT = "input_edited.csv"
@@ -32,6 +33,9 @@ def load_input(filename):
                     if key == "GDP ($ per capita) dollars":
                         split = row[key].split()
                         row[key] = split[0]
+                    if key == "Infant mortality (per 1000 births)":
+                        row[key] = row[key].replace(",", ".")
+                        row[key] = float(row[key])
                     attribute_dict[key] = row[key]
                 else:
                     attribute_dict[key] = 0
@@ -69,20 +73,43 @@ def calculate_gdp(df):
     print(mean)
     # mode is most common gdp
     counter = {}
+    list_1 = []
     for i in range(len(df.index)):
+        list_1.append(int(gdp.iloc[i]))
         if gdp.iloc[i] in counter:
             counter[gdp.iloc[i]] += 1
         else:
             counter[gdp.iloc[i]] = 1
     mode = max(counter, key=counter.get)
     print(mode)
+    return list_1
 
 
+def calculate_mortality(df):
+    mortality = df.ix[:, "Infant mortality (per 1000 births)"]
+    mortality = mortality.sort_values()
+    return mortality
 
-    # mode =
+
+def plot(gdp, mortality):
+
+    plt.subplot(2, 1, 1)
+    plt.hist(gdp, density=True, bins=30)
+    plt.title('GDP, MORTALITY')
+    plt.xlabel('GDP')
+    plt.ylabel("Amount of countries")
+
+    plt.subplot(2, 1, 2)
+    plt.boxplot(mortality)
+    plt.ylabel('MORTALITY')
+
+    plt.show()
+
 
 if __name__ == "__main__":
     attribute_list = load_input(RAW_INPUT)
     edit_input("input_edited",  attribute_list)
     df = set_pandas(INPUT)
-    calculate_gdp(df)
+    gdp = calculate_gdp(df)
+    mortality = calculate_mortality(df)
+    plot(gdp, mortality)
